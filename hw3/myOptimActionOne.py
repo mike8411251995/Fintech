c17e7bd1b19ef407b8c0199a342c7d08f7d8f745
Mike Lee <mike8411251995@yahoo.com.tw>
2019-11-19 14:13:22 +0800

    myOptimActionOne.py done

--- hw3/myOptimActionOne.py
+++ hw3/myOptimActionOne.py
@@ -4,18 +4,38 @@ import operator
 def myOptimActionOne(priceVec, transFeeRate):
 	dataLen = len(priceVec)
 	actionVec = np.zeros(dataLen)
-	conCount = 3
-	for ic in range(dataLen):
-		if ic + conCount + 1 > dataLen:
+	stockVec = np.zeros(dataLen)
+	cashVec = np.zeros(dataLen)
+	stockDecision = np.zeros(dataLen)
+	cashDecision = np.zeros(dataLen)
+
+	cashVec[0] = 1.0
+	stockVec[0] = cashVec[0] / priceVec[0]
+	for i in range(dataLen):
+		if i == 0:
 			continue
-		if all(x > 0 for x in list(map(operator.sub,priceVec[ic+1:ic+1+conCount], priceVec[ic:ic+conCount]))):
-			actionVec[ic] = 1
-		if all(x < 0 for x in list(map(operator.sub,priceVec[ic+1:ic+1+conCount], priceVec[ic:ic+conCount]))):
-			actionVec[ic] = -1
-	prevAction = -1
-	for ic in range(dataLen):
-		if actionVec[ic] == prevAction:
-			actionVec[ic] = 0
-		elif actionVec[ic] == -prevAction:
-			prevAction = actionVec[ic]
+
+		stockValues = [stockVec[i - 1], (cashVec[i - 1] / priceVec[i]) * (1.0 - transFeeRate)]
+		stockDecision[i], stockVec[i] = max(enumerate(stockValues), key=operator.itemgetter(1))
+
+		cashValues = [cashVec[i - 1], (stockVec[i - 1] * priceVec[i]) * (1.0 - transFeeRate)]
+		cashDecision[i], cashVec[i] = max(enumerate(cashValues), key=operator.itemgetter(1))
+
+	
+	# 1: stock,  -1: cash
+	indicator = 1 if stockVec[-1] > cashVec[-1] else -1
+	for i in reversed(range(dataLen)):
+		if indicator == 1:
+			if stockDecision[i] == 0:
+				actionVec[i] = 0
+			elif stockDecision[i] == 1: # from cash to stock (buy)
+				actionVec[i] = 1
+				indicator *= -1
+		elif indicator == -1:
+			if cashDecision[i] == 0:
+				actionVec[i] = 0
+			elif cashDecision[i] == 1: # from stock to cash (sell)
+				actionVec[i] = -1
+				indicator *= -1
+
 	return actionVec
