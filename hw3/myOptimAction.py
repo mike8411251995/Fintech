f147cbb97a104448366dbffe02dd388e086eb9b5
Mike Lee <mike8411251995@yahoo.com.tw>
2019-11-20 22:25:48 +0800

    hw3 done

--- hw3/myOptimAction.py
+++ hw3/myOptimAction.py
@@ -1,78 +1,69 @@
 import numpy as np
+import operator
 
 def myOptimAction(priceMat, transFeeRate):
     # Explanation of my approach:
-	# 1. Technical indicator used: Watch next day price
-	# 2. if next day price > today price + transFee ==> buy
-    #       * buy the best stock
-	#    if next day price < today price + transFee ==> sell
-    #       * sell if you are holding stock
-    # 3. You should sell before buy to get cash each day
-    # default
-    cash = 1000
-    hold = 0
-    # user definition
-    nextDay = 1
-    dataLen, stockCount = priceMat.shape  # day size & stock count   
-    stockHolding = np.zeros((dataLen,stockCount))  # Mat of stock holdings
-    actionMat = []  # An k-by-4 action matrix which holds k transaction records.
+    # 1. Technical indicator used: dynamic programming
+    # 2. For each day, record the "best previous actions" 
+    #    for each stock (from cash or other stocks) by
+    #    choosing maximized profit
+    # 3. Choose the maximum profit achieved on the last
+    #    day and select it as the starting point
+    # 4. Follow the record of "best previous actions" to
+    #    decide how to sell/buy stocks
     
-    for day in range( 0, dataLen-nextDay ) :
-        dayPrices = priceMat[day]  # Today price of each stock
-        nextDayPrices = priceMat[ day + nextDay ]  # Next day price of each stock
-        
-        if day > 0:
-            stockHolding[day] = stockHolding[day-1]  # The stock holding from the previous action day
-        
-        buyStock = -1  # which stock should buy. No action when is -1
-        buyPrice = 0  # use how much cash to buy
-        sellStock = []  # which stock should sell. No action when is null
-        sellPrice = []  # get how much cash from sell
-        bestPriceDiff = 0  # difference in today price & next day price of "buy" stock
-        stockCurrentPrice = 0  # The current price of "buy" stock
-        
-        # Check next day price to "sell"
-        for stock in range(stockCount) :
-            todayPrice = dayPrices[stock]  # Today price
-            nextDayPrice = nextDayPrices[stock]  # Next day price
-            holding = stockHolding[day][stock]  # how much stock you are holding
-            
-            if holding > 0 :  # "sell" only when you have stock holding
-                if nextDayPrice < todayPrice*(1+transFeeRate) :  # next day price < today price, should "sell"
-                    sellStock.append(stock)
-                    # "Sell"
-                    sellPrice.append(holding * todayPrice)
-                    cash = holding * todayPrice*(1-transFeeRate) # Sell stock to have cash
-                    stockHolding[day][sellStock] = 0
-        
-        # Check next day price to "buy"
-        if cash > 0 :  # "buy" only when you have cash
-            for stock in range(stockCount) :
-                todayPrice = dayPrices[stock]  # Today price
-                nextDayPrice = nextDayPrices[stock]  # Next day price
-                
-                if nextDayPrice > todayPrice*(1+transFeeRate) :  # next day price > today price, should "buy"
-                    diff = nextDayPrice - todayPrice*(1+transFeeRate)
-                    if diff > bestPriceDiff :  # this stock is better
-                        bestPriceDiff = diff
-                        buyStock = stock
-                        stockCurrentPrice = todayPrice
-            # "Buy" the best stock
-            if buyStock >= 0 :
-                buyPrice = cash
-                stockHolding[day][buyStock] = cash*(1-transFeeRate) / stockCurrentPrice # Buy stock using cash
-                cash = 0
-                
-        # Save your action this day
-        if buyStock >= 0 or len(sellStock) > 0 :
-            action = []
-            if len(sellStock) > 0 :
-                for i in range( len(sellStock) ) :
-                    action = [day, sellStock[i], -1, sellPrice[i]]
-                    actionMat.append( action )
-            if buyStock >= 0 :
-                action = [day, -1, buyStock, buyPrice]
-                actionMat.append( action )
-    return actionMat
+    dataLen, stockCount = priceMat.shape
+    actionMat = []
+    stockVec = np.zeros((dataLen, stockCount + 1))
+    stockDecision = np.zeros((dataLen, stockCount + 1))
 
+    stockVec[0][stockCount] = 1000 # initialize cash
+    for j in range(stockCount):
+        stockVec[0][j] = 0 # initialize stock
     
+    for i in range(dataLen):
+        if i == 0:
+            continue
+
+        for j in range(stockCount): # stocks
+            stockValues = np.zeros(stockCount + 1)
+            for k in range(stockCount):
+                if k == j: # no buy or sell
+                    stockValues[k] = stockVec[i-1][j]
+                else: # sell k to buy j
+                    stockValues[k] = (stockVec[i-1][k] * priceMat[i][k] / priceMat[i][j]) * (1.0 - transFeeRate) * (1.0 - transFeeRate)
+            stockValues[stockCount] = (stockVec[i-1][stockCount] / priceMat[i][j]) * (1.0 - transFeeRate) # use cash to buy j
+            stockDecision[i][j], stockVec[i][j] = max(enumerate(stockValues), key=operator.itemgetter(1))
+
+        stockValues = np.zeros(stockCount + 1) # cash
+        for k in range(stockCount):
+            # sell k to get cash
+            stockValues[k] = (stockVec[i-1][k] * priceMat[i][k]) * (1.0 - transFeeRate)
+        stockValues[stockCount] = stockVec[i-1][stockCount] # no buy or sell
+        stockDecision[i][stockCount], stockVec[i][stockCount] = max(enumerate(stockValues), key=operator.itemgetter(1))
+
+    stockDecision = stockDecision.astype(int)
+
+    # 1: stock,  -1: cash
+    indicator, _ = max(enumerate(stockVec[-1]), key=operator.itemgetter(1))
+    for i in reversed(range(dataLen)):
+        if i == 0:
+            continue
+        indicator = int(indicator)
+        if indicator == stockCount: # ... to cash
+            if stockDecision[i][indicator] == stockCount: # from cash to cash
+                indicator = stockCount
+            else: # from stock to cash (sell)
+                actionMat.insert(0, [i, stockDecision[i][indicator], -1, float("inf")])
+                indicator = stockDecision[i][indicator]
+        else: # ... to stock
+            if stockDecision[i][indicator] == stockCount: # from cash to stock
+                actionMat.insert(0, [i, -1, indicator, float("inf")])
+                indicator = stockCount
+            elif stockDecision[i][indicator] == indicator:
+                indicator = stockDecision[i][indicator]
+            else: # from stock to stock
+                actionMat.insert(0, [i, stockDecision[i][indicator], indicator, float("inf")])
+                indicator = stockDecision[i][indicator]      
+    
+    return actionMat
